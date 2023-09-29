bbcb4a63985c75ff75d832c33faf6e1aa1ec962b
jocherdevelopment@gmail.com <jose_mvp55@hotmail.com>
2023-09-29 12:22:05 -0600

    added some styles

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
       if (response.ok) {
         const data = await response.json();
         setImageSrc(data.image_url);
+        setError('');
       } else {
         const data = await response.json();
         setError(data.error);
@@ -29,17 +30,32 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Image Generator</h1>
-      <input
-        type="text"
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-        placeholder="Enter prompt"
-      />
-      <button onClick={generateImage}>Generate</button>
-      {error && <div>Error: {error}</div>}
-      {imageSrc && <img src={imageSrc} alt="Generated" />}
+    <div className="container main-container">
+      <h1 className='neon'>Image Generator</h1>
+      <div className='input-container'>
+        <div className='form'>
+          <input
+            type="text"
+            className='form-control'
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Enter prompt"
+          />
+        </div>
+        <button className="btn btn-success" onClick={generateImage}>Generate</button>
+      </div>
+      <div className="container">
+        {error && <div className='alert alert-warning'>Error: {error}</div>}
+      </div>
+      <div className='container'>
+        {imageSrc &&
+          <div className='image-container'>
+            <h4 className='neon'>Generated Image</h4>
+            <img src={imageSrc} className="image" alt="Generated" />
+          </div>
+
+        }
+      </div>
     </div>
   );
 }
